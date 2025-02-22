b4dad399a46965aed534fd4cbd54dab8b464bb17
Alex Dunlap <[omitted]>
2025-02-22 16:39:36 -0500

    Initial version of dataset downloader

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+rm ./scripts/python/smallm
+ln -s ../../smallm ./scripts/python/smallm
 python3 -m venv venv
 source ./venv/bin/activate
 python -m pip install --upgrade pip
@@ -6,3 +8,4 @@ python -m pip install glog==0.3.1
 python -m pip install black==25.1.0
 python -m pip install isort==6.0.0
 python -m pip install flake8==7.1.1
+python -m pip install pyyaml==6.0.2
--- smallm/download.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-import glog
-
-
-def download():
-    website = "https://www.gutenberg.org/cache/epub/11/pg11.txt"
-    subprocess.run(["wget", website])
-    glog.info("Download successful.")
