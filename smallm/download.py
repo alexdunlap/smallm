9a619021c74a78067ecb27b64ea5965f807a3426
Alex G. Dunlap <[omitted]>
2025-02-22 14:23:17 -0500

    Fix spelling

--- smallm/download.py
+++ smallm/download.py
@@ -6,4 +6,4 @@ import glog
 def download():
     website = "https://www.gutenberg.org/cache/epub/11/pg11.txt"
     subprocess.run(["wget", website])
-    glog.info("Download successfull.")
+    glog.info("Download successful.")
